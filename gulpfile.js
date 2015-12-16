c0772c814af0a130ebd6ff9a581185620a3b2bdb
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-12-16 00:06:16 +0000

    Add NPM, Bower, Gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var util = require('gulp-util');
+
+var onError = function (error) {
+    util.log(error);
+    this.emit('end');
+};
+
+gulp.task('build-components', function () {
+    return gulp.src('./components/**/*.js')
+        .pipe(babel())
+        .pipe(concat('components.js'))
+        .pipe(gulp.dest('.'))
+});
